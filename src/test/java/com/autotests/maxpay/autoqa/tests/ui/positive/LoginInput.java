bad71e4c15c547c7a53a39a40bc03b6995e7048e
obudilovsky <santiss@bigmir.net>
2018-07-04 16:22:19 +0300

    Change vars

--- src/test/java/com/autotests/maxpay/autoqa/tests/ui/positive/LoginInput.java
+++ src/test/java/com/autotests/maxpay/autoqa/tests/ui/positive/LoginInput.java
@@ -16,6 +16,8 @@ import org.testng.annotations.Test;
 import java.io.IOException;
 
 import static com.autotests.maxpay.autoqa.framework.SetUp.driver;
+import static com.autotests.maxpay.autoqa.framework.TestDataConstants.VALID_EMAIL;
+import static com.autotests.maxpay.autoqa.framework.TestDataConstants.VALID_PASSWORD;
 import static com.autotests.maxpay.autoqa.framework.UIPathConstants.*;
 import static com.autotests.maxpay.autoqa.framework.UIRequests.typeLoginEmailInput;
 import static org.openqa.selenium.support.ui.ExpectedConditions.invisibilityOfElementLocated;
@@ -36,7 +38,7 @@ public class LoginInput {
 
     @Test (groups = { "UI" })
     public void checkLoginInputValidValues() throws InterruptedException {
-        typeLoginEmailInput("qa+test+user@maxpay.com", "21TSeWbq");
+        typeLoginEmailInput(VALID_EMAIL, VALID_PASSWORD);
         WebDriverWait wait = new WebDriverWait(driver, 10);
         wait.until(invisibilityOfElementLocated(By.id(LOGIN_EMAIL_INPUT_PATH)));
         Assert.assertEquals(driver.getTitle(),"Dashboard — Merchant portal");
